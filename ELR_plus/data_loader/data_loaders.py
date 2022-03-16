0a7ba59dc0b80c7bac4c94f5d807e5fbcc74cb2c
lisenbuaa <2290963739@qq.com>
2022-03-16 22:36:18 +0800

    debug

--- ELR_plus/data_loader/data_loaders.py
+++ ELR_plus/data_loader/data_loaders.py
@@ -11,7 +11,7 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from PIL import Image
-from autoaugment import ImageNetPolicy
+from data_loader.autoaugment import ImageNetPolicy
 
 
 
